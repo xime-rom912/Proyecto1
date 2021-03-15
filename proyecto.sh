50a43f80285506ade8cc94828480cd6e6be3d399
moonormal <a334259@uach.mx>
2021-03-15 13:15:49 -0700

    Se completo el primer punto, inicie el menu secundario y modifique los permisos de acceso al documento

--- proyecto.sh
+++ proyecto.sh
@@ -4,7 +4,46 @@ a=$1
 
 if [ "$a" == "-a" ]; then
 	echo "Bienvenido a la guia rapida de Agile, para continuar seleccione un tema"
-	echo ""
+	echo "1. SCRUM"
+	echo "2. X.P."
+	echo "3. Kanban"
+	echo "4. Crystal"
+	read opcion
+	case $opcion in
+	1)
+		echo "Usted está en la sección "
+        	;;
+        2)
+        	echo "Usted está en la sección "
+        	;;
+        3)
+        	echo "Usted está en la sección"
+        	;;
+	4)
+		echo "Usted está en la sección"
+		;;
+	*)
+		echo"Selecciono una opcion incorrecta"
+	esac
+
 elif [ "$a" == "-t" ]; then
 	echo "Bienvenido a la guia rapida de metodologias tradicionales, para continuar seleccione un tema"
+	echo "1. Cascada"
+        echo "2. Espiral"
+        echo "3. Modelo V"
+	read opcion
+        case $opcion in
+        1)
+                echo "Usted está en la sección "
+                ;;
+        2)
+                echo "Usted está en la sección "
+                ;;
+        3)
+                echo "Usted está en la sección"
+                ;;
+        *)
+                echo"Selecciono una opcion incorrecta"
+        esac
+
 fi
