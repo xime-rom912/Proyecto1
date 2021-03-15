7fec9d46c0e992ce7728ce4894a28d768e860c29
a329782 <a329782@uach.mx>
2021-03-15 14:04:06 -0700

    Se agrega el segundo menu de despliegue en opciones -a

--- proyecto.sh
+++ proyecto.sh
@@ -11,19 +11,39 @@ if [ "$a" == "-a" ]; then
 	read opcion
 	case $opcion in
 	1)
-		echo "Usted está en la sección "
+		echo "Usted está en la sección SCRUM"
+		echo ""
+		echo "1. Agregar informacion."
+		echo "2. Buscar."
+		echo "3. Eliminar informacion."
+		echo "4. Leer base de informacion."
         	;;
         2)
-        	echo "Usted está en la sección "
+        	echo "Usted está en la sección X.P."
+			echo ""
+			echo "1. Agregar informacion."
+			echo "2. Buscar."
+			echo "3. Eliminar informacion."
+			echo "4. Leer base de informacion."
         	;;
         3)
-        	echo "Usted está en la sección"
+        	echo "Usted está en la sección Kanban"
+			echo ""
+			echo "1. Agregar informacion."
+			echo "2. Buscar."
+			echo "3. Eliminar informacion."
+			echo "4. Leer base de informacion."
         	;;
-	4)
-		echo "Usted está en la sección"
+		4)
+			echo "Usted está en la sección Crystal"
+			echo ""
+			echo "1. Agregar informacion."
+			echo "2. Buscar."
+			echo "3. Eliminar informacion."
+			echo "4. Leer base de informacion."
 		;;
 	*)
-		echo"Selecciono una opcion incorrecta"
+		echo "Selecciono una opcion incorrecta"
 	esac
 
 elif [ "$a" == "-t" ]; then
