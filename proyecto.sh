68a22481b84bef3274da789bb69e8e75764d6524
moonormal <a334259@uach.mx>
2021-03-15 14:28:56 -0700

    Se agrego el despliegue de opciones en -t y los menus en -a y -t

--- proyecto.sh
+++ proyecto.sh
@@ -4,6 +4,7 @@ a=$1
 
 if [ "$a" == "-a" ]; then
 	echo "Bienvenido a la guia rapida de Agile, para continuar seleccione un tema"
+	echo ""
 	echo "1. SCRUM"
 	echo "2. X.P."
 	echo "3. Kanban"
@@ -12,19 +13,59 @@ if [ "$a" == "-a" ]; then
 	case $opcion in
 	1)
 		echo "Usted está en la sección SCRUM"
-		echo ""
-		echo "1. Agregar informacion."
-		echo "2. Buscar."
-		echo "3. Eliminar informacion."
-		echo "4. Leer base de informacion."
-        	;;
-        2)
-        	echo "Usted está en la sección X.P."
 			echo ""
 			echo "1. Agregar informacion."
 			echo "2. Buscar."
 			echo "3. Eliminar informacion."
 			echo "4. Leer base de informacion."
+        		echo " "
+			read opcionSCRUM
+			case $opcionSCRUM in
+			1)
+				echo "Agregar información"
+        			;;
+       			2)
+        			echo "Buscar"
+        			;;
+        		3)
+				echo "Eliminar informacion"
+				;;
+			4)
+				echo "Leer base de información"
+				;;
+			*)
+        			echo "Selecciono una opción incorrecta"
+        			;;
+			esac
+
+		;;
+        2)
+        	echo "Usted está en la sección X.P."
+			echo ""
+                        echo "1. Agregar informacion."
+                        echo "2. Buscar."
+                        echo "3. Eliminar informacion."
+                        echo "4. Leer base de informacion."
+                        echo " "
+                        read opcionXP
+                        case $opcionXP in
+                        1)
+                                echo "Agregar información"
+                                ;;
+                        2)
+                                echo "Buscar"
+                                ;;
+                        3)
+                                echo "Eliminar informacion"
+                                ;;
+                        4)
+                                echo "Leer base de información"
+                                ;;
+                        *)
+                                echo "Selecciono una opción incorrecta"
+                                ;;
+                        esac
+
         	;;
         3)
         	echo "Usted está en la sección Kanban"
@@ -33,6 +74,26 @@ if [ "$a" == "-a" ]; then
 			echo "2. Buscar."
 			echo "3. Eliminar informacion."
 			echo "4. Leer base de informacion."
+			echo " "
+                        read opcionKanban
+                        case $opcionKanban in
+                        1)
+                                echo "Agregar información"
+                                ;;
+                        2)
+                                echo "Buscar"
+                                ;;
+                        3)
+                                echo "Eliminar informacion"
+                                ;;
+                        4)
+                                echo "Leer base de información"
+                                ;;
+                        *)
+                                echo "Selecciono una opción incorrecta"
+                                ;;
+                        esac
+
         	;;
 		4)
 			echo "Usted está en la sección Crystal"
@@ -41,9 +102,29 @@ if [ "$a" == "-a" ]; then
 			echo "2. Buscar."
 			echo "3. Eliminar informacion."
 			echo "4. Leer base de informacion."
+			echo " "
+                        read opcionCrystal
+                        case $opcionCrystal in
+                        1)
+                                echo "Agregar información"
+                                ;;
+                        2)
+                                echo "Buscar"
+                                ;;
+                        3)
+                                echo "Eliminar informacion"
+                                ;;
+                        4)
+                                echo "Leer base de información"
+                                ;;
+                        *)
+                                echo "Selecciono una opción incorrecta"
+                                ;;
+                        esac
+
 		;;
 	*)
-		echo "Selecciono una opcion incorrecta"
+		echo "Selecciono una opción incorrecta"
 	esac
 
 elif [ "$a" == "-t" ]; then
@@ -54,16 +135,91 @@ elif [ "$a" == "-t" ]; then
 	read opcion
         case $opcion in
         1)
-                echo "Usted está en la sección "
-                ;;
+                echo "Usted está en la sección Cascada"
+                echo ""
+                        echo "1. Agregar informacion."
+                        echo "2. Buscar."
+                        echo "3. Eliminar informacion."
+                        echo "4. Leer base de informacion."
+			echo " "
+                        read opcionCascada
+                        case $opcionCascada in
+                        1)
+                                echo "Agregar información"
+                                ;;
+                        2)
+                                echo "Buscar"
+                                ;;
+                        3)
+                                echo "Eliminar informacion"
+                                ;;
+                        4)
+                                echo "Leer base de información"
+                                ;;
+                        *)
+                                echo "Selecciono una opción incorrecta"
+                                ;;
+                        esac
+
+		;;
         2)
-                echo "Usted está en la sección "
+                echo "Usted está en la sección Espiral"
+			echo ""
+                        echo "1. Agregar informacion."
+                        echo "2. Buscar."
+                        echo "3. Eliminar informacion."
+                        echo "4. Leer base de informacion."
+			echo " "
+                        read opcionEspiral
+                        case $opcionEspiral in
+                        1)
+                                echo "Agregar información"
+                                ;;
+                        2)
+                                echo "Buscar"
+                                ;;
+                        3)
+                                echo "Eliminar informacion"
+                                ;;
+                        4)
+                                echo "Leer base de información"
+                                ;;
+                        *)
+                                echo "Selecciono una opción incorrecta"
+                                ;;
+                        esac
+
                 ;;
         3)
-                echo "Usted está en la sección"
-                ;;
+                echo "Usted está en la sección Modelo V"
+	                echo ""
+                        echo "1. Agregar informacion."
+                        echo "2. Buscar."
+                        echo "3. Eliminar informacion."
+                        echo "4. Leer base de informacion."
+			echo " "
+                        read opcionModeloV
+                        case $opcionModeloV in
+                        1)
+                                echo "Agregar información"
+                                ;;
+                        2)
+                                echo "Buscar"
+                                ;;
+                        3)
+                                echo "Eliminar informacion"
+                                ;;
+                        4)
+                                echo "Leer base de información"
+                                ;;
+                        *)
+                                echo "Selecciono una opción incorrecta"
+                                ;;
+                        esac
+
+		;;
         *)
-                echo"Selecciono una opcion incorrecta"
+                echo"Selecciono una opción incorrecta"
         esac
 
 fi
